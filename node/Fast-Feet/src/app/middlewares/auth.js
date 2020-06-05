89f454e0a2dc2e7a978ca5edf52658577604adbc
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-05 16:09:02 -0300

    jwt auth for recipient routes

---
+++ node/Fast-Feet/src/app/middlewares/auth.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+export default async function auth(req, res, next) {
+  try {
+    const token = req.headers['x-auth-token'];
+    console.log(req.headers);
+    if (!token) throw new Error('No token found');
+
+    await jwt.verify(token, process.env.PRIVATE_KEY);
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: error.message || error });
+  }
+}
--- node/Fast-Feet/src/routes.ts
+++ node/Fast-Feet/src/routes.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import ValidationMiddleware from './app/middlewares/validateFields';
+import AuthMiddleware from './app/middlewares/auth';
 
 const routes = express.Router();
 
 routes.post('/auth', ValidationMiddleware.auth, SessionController.auth);
+routes.use(AuthMiddleware);
 routes.post('/recipient', RecipientController.create);
 routes.get('/recipient', RecipientController.index);
 routes.put('/recipient/:id', RecipientController.update);
