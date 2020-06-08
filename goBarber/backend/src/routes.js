4fa254d3cac5c05d5b0fc3f9e93b750b01bef92b
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-08 00:24:46 -0300

    Initializing multer

--- goBarber/backend/src/routes.js
+++ goBarber/backend/src/routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import validationMiddleware from './app/middlewares/validation';
+
+import multerConfig from './config/multer';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
@@ -14,5 +19,9 @@ routes.put(
   UserController.update
 );
 routes.post('/session', validationMiddleware.Session, SessionController.store);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  res.json({ ok: true });
+});
 
 export default routes;
