37399b8027aa9fb63a8710edba7a1f5272cdd7fb
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-12 20:01:06 -0300

    Check for appointment

--- goBarber/backend/src/app/controllers/AppointmentController.js
+++ goBarber/backend/src/app/controllers/AppointmentController.js
@@ -1,7 +1,7 @@
 import User from '../models/User';
 import Appointments from '../models/Appointments';
 import * as Yup from 'yup';
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 class AppointmentController {
   async store(req, res) {
     const schema = Yup.object().shape({
@@ -19,15 +19,33 @@ class AppointmentController {
     const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
-    if (!isProvider)
+    if (!isProvider) {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'past date are not permitted' });
+    }
+
+    const checkAvailability = await Appointments.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appoint date is not available ' });
+    }
 
     const appointment = await Appointments.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
     return res.json(appointment);
   }
