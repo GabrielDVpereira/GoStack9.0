ca127d93c8aa19b97e212550d289a9e3169eaf77
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-16 18:02:14 -0300

    Config bee queue for creating jobs

--- goBarber/backend/src/app/controllers/AppointmentController.js
+++ goBarber/backend/src/app/controllers/AppointmentController.js
@@ -5,7 +5,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Appointments from '../models/Appointments';
 import Notification from '../Schemas/Notification';
-import Mail from '../../lib/ Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async store(req, res) {
@@ -135,20 +136,8 @@ class AppointmentController {
     appointment.canceled_at = new Date();
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
-    });
-    return res.json({ appointment });
+    await Queue.add(CancellationMail.key, { appointment });
+    return res.json(appointment);
   }
 }
 
