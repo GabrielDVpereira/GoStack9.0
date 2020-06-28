7d953f11220d059f8ef92cf8b97a4b0c9bee0985
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-28 15:28:57 -0300

    Config sentry

--- goBarber/backend/src/app.js
+++ goBarber/backend/src/app.js
@@ -2,18 +2,24 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
 import './database';
 // Já vai iniciar o database
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -23,6 +29,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ goBarber/backend/src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn:
+    'https://c33878699abf4bafbb06e3e463cf586e@o410596.ingest.sentry.io/5300198',
+};
